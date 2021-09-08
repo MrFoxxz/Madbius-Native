fb3b78079e1efd7eb97df927dcc2463d92c16490
Miguel Angel JC <mjaimes@madbius.com>
2021-09-08 17:23:30 -0500

    scrollview

--- screens/users/index.js
+++ screens/users/index.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
-import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
+import {ScrollView, StyleSheet, View} from 'react-native';
 import UserCard from '../../components/userCard';
 import Loader from '../../components/loader';
 
@@ -23,21 +23,21 @@ const Users = ({navigation}) => {
     return <Loader />;
   } else {
     return (
-      <View style={styles.container}>
-        <Text style={styles.tittle}>USERS</Text>
-
-        {userList.map(val => {
-          return (
-            <UserCard
-              id={val.id}
-              name={val.name}
-              lastName={val.lastname}
-              email={val.email}
-              comunnication={val.communication_channel}
-            />
-          );
-        })}
-      </View>
+      <ScrollView>
+        <View style={styles.container}>
+          {userList.map(val => {
+            return (
+              <UserCard
+                id={val.id}
+                name={val.name}
+                lastName={val.lastname}
+                email={val.email}
+                comunnication={val.communication_channel}
+              />
+            );
+          })}
+        </View>
+      </ScrollView>
     );
   }
 };
@@ -45,9 +45,6 @@ const Users = ({navigation}) => {
 const styles = StyleSheet.create({
   container: {
     margin: 20,
-    flex: 1,
-    alignContent: 'center',
-    justifyContent: 'center',
   },
   tittle: {
     alignSelf: 'center',
